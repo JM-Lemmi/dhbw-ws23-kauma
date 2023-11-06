882bdd06814df9f16d2185ba13451d826346d98b
Julian Lemmerich <jle@cirosec.de>
2023-11-06 18:55:46 +0100

    close Server gracefully

--- Aufgabe02/padding_oracle_server.py
+++ Aufgabe02/padding_oracle_server.py
@@ -15,34 +15,38 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     print("Server is listening...")
 
-    while True:
-        # accept incoming connections
-        client_socket, client_address = s.accept()
-        with client_socket:
-            print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+    try:
+        while True:
+            # accept incoming connections
+            client_socket, client_address = s.accept()
+            with client_socket:
+                print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
 
-            #client_socket.sendall(b"Welcome to the padding oracle server!\n")
+                #client_socket.sendall(b"Welcome to the padding oracle server!\n")
 
-            ciphertext = bytearray()
-            l = bytearray()
-            while len(ciphertext) < 16: ciphertext += client_socket.recv(1) # Ciphertext
-            while len(l) < 2: l += client_socket.recv(1) # l
-            l_int = int.from_bytes(l, byteorder='little')
+                ciphertext = bytearray()
+                l = bytearray()
+                while len(ciphertext) < 16: ciphertext += client_socket.recv(1) # Ciphertext
+                while len(l) < 2: l += client_socket.recv(1) # l
+                l_int = int.from_bytes(l, byteorder='little')
 
-            qs = [bytearray()] * l_int
-            for i in range(0, l_int):
-                while len(qs[i]) < 16 * l_int: qs[i] += client_socket.recv(1) # q-blöcke
-            
-            print(f"Received all data from {client_address[0]}:{client_address[1]}, unpadding now...")
+                qs = [bytearray()] * l_int
+                for i in range(0, l_int):
+                    while len(qs[i]) < 16 * l_int: qs[i] += client_socket.recv(1) # q-blöcke
+                
+                print(f"Received all data from {client_address[0]}:{client_address[1]}, unpadding now...")
 
-            for q in qs:
-                dc = xor(ciphertext, key)
-                xor(dc, q)
+                for q in qs:
+                    dc = xor(ciphertext, key)
+                    xor(dc, q)
 
-                try:
-                    unpadder = padding.PKCS7(128).unpadder()
-                    unpadder.update(dc)
-                except:
-                    client_socket.sendall(b'00')
-                else:
-                    client_socket.sendall(b'01')
+                    try:
+                        unpadder = padding.PKCS7(128).unpadder()
+                        unpadder.update(dc)
+                    except:
+                        client_socket.sendall(b'00')
+                    else:
+                        client_socket.sendall(b'01')
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        exit(0)

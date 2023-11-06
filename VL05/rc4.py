5c6ea622fd74a4c78ca3b52bcd44975f769d2d76
Julian Lemmerich <jle@cirosec.de>
2023-11-06 11:53:44 +0100

    RC4 first implementation

---
+++ VL05/rc4.py
@@ -0,0 +1,28 @@
+#! /bin/python3
+
+key = b"test"
+keylen = len(key)
+sbox = list(range(256))
+
+def ksa():
+    j=0
+    for i in range(256):
+        j = (j + sbox[i] + key[i % keylen]) % 256
+        sbox[i], sbox[j] = sbox[j], sbox[i]
+    
+def gen_byte(n):
+    i=0
+    j=0
+    while True:
+        i = (i+1) % 256
+        j = (j+sbox[i]) % 256
+        sbox[i], sbox[j] = sbox[j], sbox[i]
+        yield sbox[(sbox[i]+sbox[j]) % 256]
+
+i = 0
+for b in gen_byte(1):
+    print(b)
+
+    i += 1
+    if i == 256:
+        break

876ee31d1a799cd5cd0a2aa3677fc83e2d551aa4
Julian Lemmerich <s212689@student.dhbw-mannheim.de>
2023-11-26 14:17:03 +0000

    add default values for to_bytes from python 3.11 for python3.10 compatibility

--- Aufgabe02/padding_oracle_attack.py
+++ Aufgabe02/padding_oracle_attack.py
@@ -10,7 +10,7 @@ def padright(b: bytes, l: int) -> bytes: return b + (b'\x00' * (l - len(b)))
 def pkcs7zeropad(l: int) -> bytes:
     """ Returns a valid pkcs7 padding of length l """
     if l > 16: raise ValueError("l must be <= 16")
-    return (b'\x00' * (16 - l)) + int.to_bytes(l, byteorder='little') * l
+    return (b'\x00' * (16 - l)) + int.to_bytes(l, length=1, byteorder='little') * l
 
 def attack(hostname: str, port: int, ciphertext: bytes, iv: bytes) -> bytes:
 
@@ -25,7 +25,7 @@ def attack(hostname: str, port: int, ciphertext: bytes, iv: bytes) -> bytes:
             s.sendall(b'\x00\x01') # 256 q blocks (0x0100 in little endian)
 
             for i in range(256):
-                q_short = padleft(int.to_bytes(i), 16-p) # counting 1-256, filled from the left with 0
+                q_short = padleft(int.to_bytes(i, length=1, byteorder='big'), 16-p) # counting 1-256, filled from the left with 0
                 dc_p = xor(pkcs7zeropad(p+1), dc)[16-p:] # only the right bytes of dc with the correct padding xor.
                 q = q_short + dc_p
                 assert len(q) == 16
@@ -40,12 +40,12 @@ def attack(hostname: str, port: int, ciphertext: bytes, iv: bytes) -> bytes:
                 if t == 1:
                     if p == 15:
                         # correct, no false positive possible
-                        logging.debug(f"Byte {int.to_bytes(i, byteorder='little')} at position {p} is correct (before xor with padding)")
-                        dc[16-1-p] = xor(padleft(int.to_bytes(i, byteorder='big'), 16-p), pkcs7zeropad(p+1))[16-1-p] # set the correct byte in the zeroiv
+                        logging.debug(f"Byte {int.to_bytes(i, length=1, byteorder='little')} at position {p} is correct (before xor with padding)")
+                        dc[16-1-p] = xor(padleft(int.to_bytes(i, length=1, byteorder='big'), 16-p), pkcs7zeropad(p+1))[16-1-p] # set the correct byte in the zeroiv
                     else:
                         # Checking for false positive
                         # repeating code from ln 28ff.
-                        q_short = padleft(b'\xff' + int.to_bytes(i), 16-p) # added 0xff (inverse of 0x00) left of the currently counting number
+                        q_short = padleft(b'\xff' + int.to_bytes(i, length=1, byteorder='big'), 16-p) # added 0xff (inverse of 0x00) left of the currently counting number
                         dc_p = xor(pkcs7zeropad(p+1), dc)[16-p:]
                         q = q_short + dc_p
                         assert len(q) == 16
@@ -55,11 +55,11 @@ def attack(hostname: str, port: int, ciphertext: bytes, iv: bytes) -> bytes:
                         sucdouble = s.recv(1)
                         if sucdouble == b'\x01':
                             # true positive
-                            logging.debug(f"Byte {int.to_bytes(i, byteorder='little')} at position {p} is correct (before xor with padding)")
-                            dc[16-1-p] = xor(padleft(int.to_bytes(i, byteorder='big'), 16-p), pkcs7zeropad(p+1))[16-1-p] # set the correct byte in the zeroiv
+                            logging.debug(f"Byte {int.to_bytes(i, length=1, byteorder='little')} at position {p} is correct (before xor with padding)")
+                            dc[16-1-p] = xor(padleft(int.to_bytes(i, length=1, byteorder='big'), 16-p), pkcs7zeropad(p+1))[16-1-p] # set the correct byte in the zeroiv
                         else:
                             # false positive
-                            logging.debug(f"Byte {int.to_bytes(i, byteorder='little')} at position {p} is wrong (false positive)")
+                            logging.debug(f"Byte {int.to_bytes(i, length=1, byteorder='little')} at position {p} is wrong (false positive)")
 
                     
                 elif t == 0:
